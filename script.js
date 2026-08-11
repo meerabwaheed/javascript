e44f8c65bb94b5ba0afd9c476e8588c130fcb1d8
Meerab Waheed <devmeerab@gmail.com>
2026-08-11 05:13:36 +0500

    concepts of arithematic operations

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log("hello world!");
+//arithematic operators
+let a = 10;
+let b = 5;
+let c = a + b;
+console.log("a + b = " + c);
+console.log("a - b = " + (a - b));
+console.log("a =",a ,"& b =",b);
