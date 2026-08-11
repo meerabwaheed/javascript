7ff8c5cac22d8c2dffdaa13f0bc9ea7a63d4b6fa
Meerab Waheed <devmeerab@gmail.com>
2026-08-11 05:15:41 +0500

    works on some modules

--- script.js
+++ script.js
@@ -6,3 +6,6 @@ let c = a + b;
 console.log("a + b = " + c);
 console.log("a - b = " + (a - b));
 console.log("a =",a ,"& b =",b);
+console.log("a * b = " + (a * b));
+console.log("a / b = " + (a / b));
+console.log("a % b = " + (a % b));
