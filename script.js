c4c67b99e85e6af291e411d37a450027ed47a338
Meerab Waheed <devmeerab@gmail.com>
2026-08-11 05:32:34 +0500

    assignment operators are assig to some extent

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
-console.log("hello world!");
-//arithematic operators
-let a = 10;
-let b = 5;
-let c = a + b;
-console.log("a + b = " + c);
-console.log("a - b = " + (a - b));
-console.log("a =",a ,"& b =",b);
-console.log("a * b = " + (a * b));
-console.log("a / b = " + (a / b));
-console.log("a % b = " + (a % b));
-console.log("a % b = " + (a % b));
-console.log("a ** b = " + (a ** b));
+// console.log("hello world!");
+// //arithematic operators
+// let a = 10;
+// let b = 5;
+// let c = a + b;
+// console.log("a + b = " + c);
+// console.log("a - b = " + (a - b));
+// console.log("a =",a ,"& b =",b);
+// console.log("a * b = " + (a * b));
+// console.log("a / b = " + (a / b));
+// console.log("a % b = " + (a % b));
+// console.log("a % b = " + (a % b));
+// console.log("a ** b = " + (a ** b));
 //unary operators
-let x = 10;
-let y = 5;
-console.log("x =",x,"& y =",y);
-a = a - 1;
+// let x = 10;
+// let y = 5;
+// console.log("x =",x,"& y =",y);
+// a = a - 1;
+// console.log("a =",a);
+
+//assignment operators
+let a = 5;
+let b = 2;
+a += 4;
 console.log("a =",a);
