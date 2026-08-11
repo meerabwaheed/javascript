09662204381a099eb1de910e6dd079c03a5c8dda
Meerab Waheed <devmeerab@gmail.com>
2026-08-11 06:10:43 +0500

    logical OR || operator is used

--- script.js
+++ script.js
@@ -34,8 +34,15 @@
 // let y = 5;  
 // conole.log("x > y && x < y",x > y && x < y);
 
+// let a = 6;
+// let b = 5;
+// let cond1 = a >= b;
+// let cond2 = a === b;
+// console.log("cond1 && cond2", cond1 && cond2);
+
+//logical OR || operator
 let a = 6;
 let b = 5;
 let cond1 = a >= b;
 let cond2 = a === b;
-console.log("cond1 && cond2", cond1 && cond2);
+console.log("cond1 || cond2", cond1 || cond2);
