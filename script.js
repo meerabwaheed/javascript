51c385e0c7dc0ca26cd52fb3ca29380b2f3a4886
Meerab Waheed <devmeerab@gmail.com>
2026-08-16 14:11:00 +0500

    Refactor logical operators and conditional statements in script.js

--- script.js
+++ script.js
@@ -41,19 +41,19 @@
 // console.log("cond1 && cond2", cond1 && cond2);
 
 //logical OR || operator
-// let a = 6;
-// let b = 5;
-// let cond1 = a <= b;
-// let cond2 = a === b;
-// console.log("cond1 || cond2", cond1 || cond2);
-// //logical NOT ! operator
+let a = 6;
+let b = 5;
+let cond1 = a <= b;
+let cond2 = a === b;
+console.log("cond1 || cond2", cond1 || cond2);
+//logical NOT ! operator
 // console.log("cond1 != cond2", cond1 != cond2);
 
 //conditional statements
-let age = 18;
-if(age >= 18){
-console.log("you are eligible for voting");
-}else{
+// let age = 18;
+// if(age >= 18){
+// console.log("you are eligible for voting");
+// }else{
 //     console.log("you are not eligible for voting");
 // }
 
@@ -79,17 +79,17 @@ console.log("you are eligible for voting");
 // alert("hell0o world");//one time popup
 // prompt("what is your name?");//one time popup with input
 
-let str = "umar";
-let newStr = str.toLowerCase();
-console.log(newStr);
-let str = "      meer raj   umar";
-console.log(str.trim());
- let fullName =prompt ("enter your full name without spaces:");
- let userName = "@" + fullName + fullName.length;
- conosle.log(userName);
-let marks = [1,2,3,4,5];
-console.log(marks);
-console.log(marks.length);
+// let str = "umar";
+// let newStr = str.toLowerCase();
+// console.log(newStr);
+// let str = "      meer raj   umar";
+// console.log(str.trim());
+//  let fullName =prompt ("enter your full name without spaces:");
+//  let userName = "@" + fullName + fullName.length;
+//  conosle.log(userName);
+// let marks = [1,2,3,4,5];
+// console.log(marks);
+// console.log(marks.length);
 
-let heroes = ["ironman","umar","spiderman","batsman"];
-console.log(heroes);
+// let heroes = ["ironman","umar","spiderman","batsman"];
+// console.log(heroes);
