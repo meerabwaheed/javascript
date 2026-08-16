3b9dfb9bd4024251ffe36a07c3991437e045d975
Meerab Waheed <devmeerab@gmail.com>
2026-08-16 13:59:23 +0500

    Add string manipulation examples and console logs

--- script.js
+++ script.js
@@ -78,3 +78,18 @@
 
 // alert("hell0o world");//one time popup
 // prompt("what is your name?");//one time popup with input
+
+let str = "umar";
+let newStr = str.toLowerCase();
+console.log(newStr);
+let str = "      meer raj   umar";
+console.log(str.trim());
+ let fullName =prompt ("enter your full name without spaces:");
+ let userName = "@" + fullName + fullName.length;
+ conosle.log(userName);
+let marks = [1,2,3,4,5];
+console.log(marks);
+console.log(marks.length);
+
+let heroes = ["ironman","umar","spiderman","batsman"];
+console.log(heroes);
